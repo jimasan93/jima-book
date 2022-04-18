f6b9ed72fc746fdb96e561545c0c856ee0dbfd34
Yuichi Kitajima <yuichi_kitajima@f4samurai.jp>
2022-04-19 07:49:13 +0900

    jsの関数部分をリファクタリング

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    function navbarShrink() {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.remove('navbar-shrink');
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink');
         }
-
-    };
-
+    }
     // Shrink the navbar
     navbarShrink();
 
