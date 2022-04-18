969d2f12ec9cc62acf4710609bae6903de9507e5
Yuichi Kitajima <yuichi_kitajima@f4samurai.jp>
2022-04-18 19:23:00 +0900

    jsで不要なコメントアウトがあったので、削除

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -23,7 +14,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
